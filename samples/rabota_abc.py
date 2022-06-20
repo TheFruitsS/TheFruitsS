3403c4ac3400b4b14eae2a7f1b7a2f20ff7e410b
TheFruitsS <dimitardzhevizov88@gmail.com>
2022-06-20 13:31:35 +0100

    continue with Fundamentals_tasks

--- samples/rabota_abc.py
+++
@@ -1,6 +0,0 @@
-location = input()
-first_letter = 'abcdefghijklmnopqrstuvwxyz'
-second_letter = 'abcdefghijklmnopqrstuvwxyz'
-for location in (first_letter, second_letter):
-    
-    print(location)
