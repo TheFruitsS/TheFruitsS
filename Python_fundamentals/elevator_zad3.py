cfc7b973dd866e795d444249daacf2cbd0dfb19d
TheFruitsS <dimitardzhevizov88@gmail.com>
2022-06-21 10:13:10 +0100

    continue with Fundamentals_tasks

--- Python_fundamentals/elevator_zad3.py
+++ Python_fundamentals/elevator_zad3.py
@@ -1,18 +1,22 @@
 import math
 number_of_people_N = int(input())
 capacity_P = int(input())
-if capacity_P == 0:
-    print(0)
+#if capacity_P == 0:
+    #print(0)
 
-elif ((number_of_people_N % capacity_P) % capacity_P) == 0:
-    print(math.floor(number_of_people_N / capacity_P))
+#elif ((number_of_people_N % capacity_P) % capacity_P) == 0:
+    #print(math.floor(number_of_people_N / capacity_P))
 
-else:
-    courses = (number_of_people_N // capacity_P) + (capacity_P % (number_of_people_N % capacity_P))
+#else:
+   # courses = (number_of_people_N // capacity_P) + (capacity_P % (number_of_people_N % capacity_P))
 
-    if courses == 0:
-        print(1)
+   # if courses == 0:
+      #  print(1)
 
-    else:
-        print(courses)
+  #  else:
+      #  print(courses)
 #return 60% success in judge system.I guess because I used a formula
+
+courses = number_of_people_N / capacity_P
+print(math.ceil(courses))
+#return 100 percent in judge system
