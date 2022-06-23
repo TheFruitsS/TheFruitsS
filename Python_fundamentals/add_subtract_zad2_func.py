69624a361339752cb4a5386cefd47a2ca7f071b4
TheFruitsS <dimitardzhevizov88@gmail.com>
2022-06-23 20:58:34 +0100

    continue with Fundamentals_tasks

---
+++ Python_fundamentals/add_subtract_zad2_func.py
@@ -0,0 +1,11 @@
+def sum_numbers(first_numbers, second_numbers):
+    return first_numbers + second_numbers
+def subtract(sum_numbers,third_number):
+    return int(sum_numbers / third_number)
+def add_and_subtract(sum_numbers, subtract):
+    print(subtract)
+
+first_numbers = int(input())
+second_numbers = int(input())
+third_number = int(input())
+print(subtract)
---
+++ Python_fundamentals/pop_my_list_function.py
@@ -0,0 +1,6 @@
+my_list_1 = [1, 2, 3, 4, 5, 2, 2, 8, "tralala", 9, 10]
+my_list_2 = my_list_1.copy()
+my_list_2.pop()
+print(my_list_2)
+print(my_list_1)
+#pop vliqe i na dvata lista,moje da e my_list2 = my list 1 shte izkara i iztrie i dvata.tova se nari4a refference
