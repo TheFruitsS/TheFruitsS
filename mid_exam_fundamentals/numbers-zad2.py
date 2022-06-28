0508d29a6985660ec6a8e0f6d1633614176656bb
TheFruitsS <dimitardzhevizov88@gmail.com>
2022-06-28 16:01:03 +0100

    continue with Fundamentals_tasks

---
+++ mid_exam_fundamentals/numbers-zad2.py
@@ -0,0 +1,10 @@
+def add(given_value):
+    return given_value
+def remove(first_occurrence,):
+    return int(first_occurrence)
+def replace(first_occurrence,given_value):
+    print(t)
+
+given_value = int(input())
+first_occurrence = int(input())
+print(t)
