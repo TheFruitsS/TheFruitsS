ece31bfcb8362d8da77042f685f8f9b02a7f2040
TheFruitsS <dimitardzhevizov88@gmail.com>
2022-07-08 10:41:33 +0100

    continue with Fundamentals_tasks

--- Python_fundamentals/add_subtract_zad2_func.py
+++ Python_fundamentals/add_subtract_zad2_func.py
@@ -1,11 +1,13 @@
-def sum_numbers(first_numbers, second_numbers):
-    return first_numbers + second_numbers
-def subtract(sum_numbers,third_number):
-    return int(sum_numbers / third_number)
-def add_and_subtract(sum_numbers, subtract):
-    print(subtract)
-
+def sum_numbers(first, second):
+    return first + second
+def subtract(sum, third_number):
+    return sum - third_number
+def add_and_subtract(first, second, third):
+    sum_of_first_and_second_integers = sum_numbers(first, second)
+    result = subtract(sum_of_first_and_second_integers, third)
+    return result
+#Important every functions have their own parameters def sum numbers (first and second) is not the same as the others they have another first and seconds parameters
 first_numbers = int(input())
 second_numbers = int(input())
 third_number = int(input())
-print(subtract)
+print(add_and_subtract(first_numbers, second_numbers,third_number))
