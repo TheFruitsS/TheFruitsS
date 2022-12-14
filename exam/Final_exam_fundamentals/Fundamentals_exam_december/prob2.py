885418b31306aad575195b8ec26264b7e63f249e
dimi&vili <dimitardzhevizov88@gmail.com>
2022-12-14 00:13:42 +0000

    continue with Fundamentals_tasks

--- exam/Final_exam_fundamentals/Fundamentals_exam_december/prob2.py
+++ exam/Final_exam_fundamentals/Fundamentals_exam_december/prob2.py
@@ -1,16 +1,17 @@
 import re
 data = int(input())
-
-
+count = []
 for _ in range(data):
     str_in = input()
     str_pattern = r'U\$([A-Za-z]{3,})U\$P\@\$([A-Za-z]{5,}\d{1,})P\@\$'
     #{3,}in this pattern regex find 3 or more matches if was only{3} that will match exactly 3 matches not less not more
     str_valid = re.search(str_pattern, str_in)
 
+
     if str_valid:
         username, password = str_valid.groups()
-        #count_matches = username.count(username)
+        count.append(username)
+
 
         print(f'Registration was successful Username: {username}, Password: {password}')
         #exit(print(f"Successful registrations: {count_matches}"))
@@ -18,5 +19,6 @@ for _ in range(data):
     else:
         print(f"Invalid username or password")
 
-    for count_matches in str_valid:
-        count_matches
+print(f"Successful registrations: {len(count)}")
+
+
