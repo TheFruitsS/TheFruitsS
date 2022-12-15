eee03693a4ea96b2e52eceb003dbb484c9676be2
dimi&vili <dimitardzhevizov88@gmail.com>
2022-12-15 09:16:33 +0000

    continue with Fundamentals_tasks

--- exam/Final_exam_fundamentals/Fundamentals_exam_december/prob2.py
+++ exam/Final_exam_fundamentals/Fundamentals_exam_december/prob2.py
@@ -6,15 +6,11 @@ for _ in range(data):
     str_pattern = r'U\$([A-Za-z]{3,})U\$P\@\$([A-Za-z]{5,}\d{1,})P\@\$'
     #{3,}in this pattern regex find 3 or more matches if was only{3} that will match exactly 3 matches not less not more
     str_valid = re.search(str_pattern, str_in)
-
-
     if str_valid:
         username, password = str_valid.groups()
         count.append(username)
 
-
         print(f'Registration was successful Username: {username}, Password: {password}')
-        #exit(print(f"Successful registrations: {count_matches}"))
 
     else:
         print(f"Invalid username or password")
--- exam/Final_exam_fundamentals/add_astrounaut.py
+++ exam/Final_exam_fundamentals/add_astrounaut.py
@@ -1,41 +1,26 @@
-def additional_print_function(astronaut_dict):
-    for key in astronaut_dict:
-        value_best_before = astronaut_dict[key][0]
-        value1_nutrition_dict = astronaut_dict[key][1]
-        print(f"Item: {key}, Best before: {value_best_before}, Nutrition: {value1_nutrition_dict}")
-
-
 def astronaut_prob2():
     import re
-
+    
     data = input()
-    foods_str = r'[#|][A-z]+\b\D+[A-z]+[#|]\d{2}/\d{2}/\d{2}[#|]\d+|[#|][A-z]+[#|]\d{2}/\d{2}/\d{2}[#|]\d+'
-    result_food = re.findall(foods_str, data).__str__()
+    foods_pattern = r'[\#\|]([A-Z\sa-z]+)[\#\|](\d{2}\/\d{2}\/\d{2})[\#\|](\d+)[\#\|]'
+    result_food = re.findall(foods_pattern, data)
 
-    food_pattern = r'\w+[A-z]\D+[A-z]'
-    food = re.findall(food_pattern, result_food)
+    if result_food:
 
+        calories_list = 0
 
-    data_pattern = r'\d{2}/\d{2}/\d{2}'
-    best_before = re.findall(data_pattern, data)
+        for food, best_before, calories in result_food:
+            food_dict = {food: [best_before, calories]}
+            calories_list += int(food_dict[food][1])
 
-    nutrition_pattern = r'[#|]\D+[#|]\d{2}/\d{2}/\d{2}[#|]'
-    nutrition_str = re.split(nutrition_pattern, result_food).__str__()
-
-
-    cal_pattern = r'\d+'
-    calories = re.findall(cal_pattern, nutrition_str)
-
-    calories_food = sum(list(map(int, calories)))
-
-    astronaut_food = {food[i]: [best_before[i], calories[i]] for i in range(0, len(food))}
-    if calories_food > 2000:
-        days = calories_food // 2000
-        print(f'You have food to last you for: {days} days!')
-        additional_print_function(astronaut_food)
-    else:
-        print("You have food to last you for: 0 days!")
-
-
+        if calories_list > 2000:
+            days = calories_list // 2000
+            print(f"You have food to last you for: {days} days!")
+            for food, best_before, calories in result_food:
+                food_dict = {food: [best_before, calories]}
+                print(f"Item: {food}, Best before: {food_dict[food][0]}, Nutrition: {food_dict[food][1]}")
+        else:
+            print("You have food to last you for: 0 days!")
+ 
 
 astronaut_prob2()
