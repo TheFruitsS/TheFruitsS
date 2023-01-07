c967f4a9caacda951382733ff8945f222c008037
TheFruitsS <89266279+TheFruitsS@users.noreply.github.com>
2023-01-07 22:51:33 +0000

    Update Problem_2_Destination_Mapper.py

--- exam/Final_exam_fundamentals/Problem_2_Destination_Mapper.py
+++ exam/Final_exam_fundamentals/Problem_2_Destination_Mapper.py
@@ -1,31 +1,20 @@
-def list_of_lists(txtli):
-    list1 = []
-    lenght = 0
-    for x in txtli:
-        list1.extend(x)
-        for y in x:
-
-            lenght += len(y)
-    result = ', '.join(list1)
-    print(f"Destinations: {result}")
-    print(f"Travel Points: {lenght}")
-
 def destination_map():
     import re
     data = input()
-    pattern = r'\=[A-Z][a-z]{2,}\=|\=[A-Z][a-z]{2,}\s[A-Za-z]+\=|\/[A-Z][a-z]{2,}\/|\/[A-Z][a-z]{2,}\s[A-Za-z]+\/'
+    pattern = r'\=[A-Z][A-Za-z]{2,}\=|\=[A-Z][A-Za-z]{2,}\s[A-Za-z]+\=|\/[A-Z][A-Za-z]{2,}\/|\/[A-Z][A-Za-z]{2,}\s[A-Za-z]+\/'
     matches = re.findall(pattern, data)
-    result = ''
+    
+    cleli = []
     lenght = 0
-    if matches:
+    for strma in matches:
+      cle = strma[1:-1]
+      lenght += len(cle)
+      cleli.append(cle)
+      
+    
+    result = ', '.join(cleli)
+    print(f"Destinations: {result}")
+    print(f"Travel Points: {lenght}")
 
-        txtli = [((x.replace('=', ',').replace('/', ','))[1:-1]).split(',')
-                 for x in matches]
-
-        list_of_lists(txtli)
-
-    else:
-        print(f"Destinations: {result}")
-        print(f"Travel Points: {lenght}")
 
 destination_map()
