eec92c0cb61fb6bc6df318bc2aaf6c7b70474b92
TheFruitsS <dimitardzhevizov88@gmail.com>
2023-04-10 18:15:47 +0100

    continue with Fundamentals_tasks

--- exam/Final_exam_fundamentals/zad2.py
+++ exam/Final_exam_fundamentals/zad2.py
@@ -9,9 +9,9 @@ for _ in range(data_times+1):
     datas = input()
     data1.append(datas)
 
-    regex = r'(^[A-Z][a-z]{3,}\s[A-Z][a-z]{3,})#{1,}([A-Z][a-z]+[[&]?[[A-z]+]?){0,2}(\d{2}[A-z]+\d*\s{1}[JSC|Ltd.]+)'
+    regex = r'(^[A-Z][a-z]{3,}\s[A-Z][a-z]{3,})#{1,}([A-z]+&?[[A-Za-z]+]?&?[A-Za-z]+)(\d{2}\w*\s*\d*[Ltd.|JSC]+)'
 
-    data_process = re.search(regex, datas).groups()
+    data_process = re.findall(regex, datas)
     if data_process:
         print(data_process)
     else:
